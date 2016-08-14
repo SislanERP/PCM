b9f5c3a67508be5367968dbba24af53d14a99065
Alex Pacheco <alex.pacheco.s@gmail.com>
2016-08-14 05:14:46 -0500

    ISSUE #1 Inplemented a basic login with gen Devise

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!
+  
   def index
     @mainTitle = "Welcome in PCM Software Project"
     @mainDesc = "It is an application skeleton for a typical Ruby on Rails web app. You can use it to quickly bootstrap your webapp projects and dev/prod environment."
