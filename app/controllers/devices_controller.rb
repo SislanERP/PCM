0f4221964dfc506123befef1cf34431f1b6b5a1b
Alex Pacheco <alex.pacheco.s@gmail.com>
2016-09-08 20:20:59 -0300

    ISSUE #17 Optimizing the show of device

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -10,11 +10,13 @@ class DevicesController < ApplicationController
   def show
     @device = Device.includes(:device_supplies).find(params[:id])
 
-    @instrunents = @device.device_supplies.includes(:instrument_type, :supply).where.not(instrument_type_id: nil)
+    @system_devices = @device.system.devices.where.not(id: @device.id)
 
-    @lubricants = @device.device_supplies.includes(:supply).where(lubrication: true)
+    @device_supplies_instrunents = @device.device_supplies.includes(:instrument_type, :supply).where.not(instrument_type_id: nil)
 
-    @supplies = @device.device_supplies.includes(:supply).where(lubrication: false, instrument_type_id: nil)
+    @device_supplies_lubricants = @device.device_supplies.includes(:supply).where(lubrication: true)
+
+    @device_supplies_supplies = @device.device_supplies.includes(:supply).where(lubrication: false, instrument_type_id: nil)
 
     # render :layout => "layout_show"
   end
