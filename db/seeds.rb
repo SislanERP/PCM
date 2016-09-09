7e65eae536b285d7b431369e841dbf871c83d957
Alex Pacheco <alex.pacheco.s@gmail.com>
2016-09-09 15:42:23 -0300

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,34 @@
 
 DeviceSupply.destroy_all
+ActiveRecord::Base.connection.execute("ALTER SEQUENCE device_supplies_id_seq RESTART WITH 1")
+
 Supply.destroy_all
+ActiveRecord::Base.connection.execute("ALTER SEQUENCE supplies_id_seq RESTART WITH 1")
+
 InstrumentType.destroy_all
+ActiveRecord::Base.connection.execute("ALTER SEQUENCE instrument_types_id_seq RESTART WITH 1")
+
 Item.destroy_all
+ActiveRecord::Base.connection.execute("ALTER SEQUENCE items_id_seq RESTART WITH 1")
 
 Device.destroy_all
+ActiveRecord::Base.connection.execute("ALTER SEQUENCE devices_id_seq RESTART WITH 1")
 Provider.destroy_all
+ActiveRecord::Base.connection.execute("ALTER SEQUENCE providers_id_seq RESTART WITH 1")
 DeviceType.destroy_all
+ActiveRecord::Base.connection.execute("ALTER SEQUENCE device_types_id_seq RESTART WITH 1")
 System.destroy_all
+ActiveRecord::Base.connection.execute("ALTER SEQUENCE systems_id_seq RESTART WITH 1")
 Plant.destroy_all
+ActiveRecord::Base.connection.execute("ALTER SEQUENCE plants_id_seq RESTART WITH 1")
 Branch.destroy_all
+ActiveRecord::Base.connection.execute("ALTER SEQUENCE branches_id_seq RESTART WITH 1")
 User.destroy_all
+ActiveRecord::Base.connection.execute("ALTER SEQUENCE users_id_seq RESTART WITH 1")
 Company.destroy_all
+ActiveRecord::Base.connection.execute("ALTER SEQUENCE companies_id_seq RESTART WITH 1")
 Role.destroy_all
+ActiveRecord::Base.connection.execute("ALTER SEQUENCE roles_id_seq RESTART WITH 1")
 
 AdminUser.destroy_all
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
