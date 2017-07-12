c43246284d261bbcb0e9fe55106ebcfeb7cb80a4
Alex Pacheco <alex.pacheco.s@gmail.com>
2017-07-12 14:38:04 -0400

    change user for DigitalOcean

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for current version of Capistrano
 lock '3.6.1'
 
-set :user                    , 'deploy'
+set :user                    , 'root'
 set :application             , 'pcmapp'
 set :deploy_to               , "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :scm                     , :git
